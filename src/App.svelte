d9c8821274c17d806c8d31c39cbe0f11e2053e6a
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2024-08-24 15:55:43 +0200

    initialize svelte rewrite

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Counter from "./components/counter/Counter.svelte"
+</script>
+
+<main
+    class="flex h-screen w-screen flex-col items-center justify-center gap-4 bg-black text-white"
+>
+    <h1 class="text-2xl font-bold">Vite + Svelte</h1>
+    <Counter />
+</main>
---
+++ src/components/counter/Counter.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    let count: number = $state(0)
+    const increment = () => (count += 1)
+</script>
+
+<button
+    onclick={increment}
+    class="rounded-lg border-2 border-white bg-green-800 px-4 py-2"
+>
+    Count is {count}
+</button>
